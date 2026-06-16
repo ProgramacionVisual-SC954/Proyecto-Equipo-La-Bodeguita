d0f95813ee02cf3e6d4e33e289bde6c6827de8ea
Yadira216 <yadiracor177@gmail.com>
2026-06-16 09:50:16 -0600

    Agregue Empleado.vb y Gerente.vb (#3)

--- LaBodeguita/obj/Debug/net8.0-windows/LaBodeguita.AssemblyInfo.vb
+++ LaBodeguita/obj/Debug/net8.0-windows/LaBodeguita.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("LaBodeguita"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a3d3c0125ca4ab8c57599bc727a70cfe01e4ad5"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("LaBodeguita"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("LaBodeguita"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
