7b91bdc01d404b7d031e7925451e2bcef91cb82c
Yadira216 <yadiracor177@gmail.com>
2026-06-17 14:03:43 -0400

    modifique empleado (#7)
    agregando rfc

--- LaBodeguita/Empleado.vb
+++ LaBodeguita/Empleado.vb
@@ -1,14 +1,20 @@
 Public MustInherit Class Empleado
+
     Private _nombre As String
     Private _apellido As String
-    Private _id As String
+    Private _rfc As String
     Private _salarioBase As Decimal
 
-    Public Sub New(nombre As String, apellido As String, id As String, salarioBase As Decimal)
+    Public Sub New(nombre As String,
+                   apellido As String,
+                   rfc As String,
+                   salarioBase As Decimal)
+
         _nombre = nombre
         _apellido = apellido
-        _id = id
+        _rfc = rfc
         _salarioBase = salarioBase
+
     End Sub
 
     Public Property Nombre As String
@@ -29,12 +35,12 @@ Public MustInherit Class Empleado
         End Set
     End Property
 
-    Public Property Id As String
+    Public Property RFC As String
         Get
-            Return _id
+            Return _rfc
         End Get
         Set(value As String)
-            _id = value
+            _rfc = value
         End Set
     End Property
 
@@ -48,5 +54,15 @@ Public MustInherit Class Empleado
     End Property
 
     Public MustOverride Function CalcularPagoMensual() As Decimal
-    Public MustOverride Function ObtenerFicha() As String
+
+    Public Overridable Function ObtenerFicha() As String
+
+        Return "Nombre: " & Nombre &
+               vbCrLf & "Apellido: " & Apellido &
+               vbCrLf & "RFC: " & RFC &
+               vbCrLf & "Salario Base: " &
+               ModuloValidaciones.FormatearMoneda(SalarioBase)
+
+    End Function
+
 End Class
