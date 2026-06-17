0f48dd5c353169ba3af338b07257c22bd62c2b4a
Crysssss-1 <dcrystal13@hotmail.com>
2026-06-17 11:21:50 -0600

    Feature/clase operario (#5)
    * Agregue Empleado.vb y Gerente.vb
    
    * Agregar operario
    
    ---------
    
    Co-authored-by: Yadira Osmara C <yadiracor177@gmail.com>
    Co-authored-by: Noveno <Noveno@ADMIN>
    Co-authored-by: Lau <ctlaura1210@gmail.com>

---
+++ LaBodeguita/Operario.vb
@@ -0,0 +1,55 @@
+﻿Imports System.Runtime.InteropServices.JavaScript.JSType
+
+Public Class Operario
+    Inherits Empleado
+
+    Private _turno As String
+    Private _horasExtra As Integer
+
+    Public Sub New(nombre As String, apellido As String, id As String, salarioBase As Decimal, turno As String, horasExtra As Integer)
+        MyBase.New(nombre, apellido, id, salarioBase)
+
+        Me.Turno = turno
+        Me.HorasExtra = horasExtra
+    End Sub
+
+    Public Property Turno As String
+        Get
+            Return _turno
+        End Get
+        Set(value As String)
+            If value = "Matutino" OrElse value = "Vespertino" OrElse value = "Nocturno" Then
+                _turno = value
+            Else
+                Throw New Exception("Turno inválido")
+            End If
+        End Set
+    End Property
+
+    Public Property HorasExtra As Integer
+        Get
+            Return _horasExtra
+        End Get
+        Set(value As Integer)
+            If value >= 0 Then
+                _horasExtra = value
+            Else
+                Throw New Exception("Horas extra no pueden ser negativas")
+            End If
+        End Set
+    End Property
+
+    Public Overrides Function CalcularPagoMensual() As Decimal
+        Return SalarioBase + (_horasExtra * 100D)
+    End Function
+
+    Public Overrides Function ObtenerFicha() As String
+        Return "OPERARIO: " & Nombre & " " & Apellido &
+               " | ID: " & Id &
+               " | Salario: " & SalarioBase.ToString("C") &
+               " | Turno: " & _turno &
+               " | Horas Extra: " & _horasExtra &
+               " | Pago Mensual: " & CalcularPagoMensual().ToString("C")
+    End Function
+
+End Class
